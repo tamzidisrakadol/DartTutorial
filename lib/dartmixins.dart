6f11bea8af2492ad91d1c9d6f2e493aa374c9dfb
Tamzid Israk Adol <tamzidisrak12@gmail.com>
2025-05-27 03:04:20 +0600

    Learn about mixin and mixin class;

---
+++ lib/dartmixins.dart
@@ -0,0 +1,50 @@
+
+void main(){
+  UserInfo userInfo = UserInfo("Tamzid","Ahmed");
+  userInfo.checkUser1("Elon");
+
+  Event event = Event("PC house");
+  event.log("Malware function");
+
+
+}
+
+mixin UserLogging{
+
+  void log(String message){
+      print("Hey $message");
+  }
+
+  void checkUser(String name){
+    print("Hello super $name");
+  }
+
+}
+
+
+class UserInfo with UserLogging{
+
+  String fname;
+  String lName;
+
+  UserInfo(this.fname,this.lName);
+
+  void checkUser1(name){  // can set different name. if name goes same then we have to use override annotation
+    super.checkUser(name); //print the mixin method
+    print("overide $name $lName"); //print the new method
+  }
+
+}
+
+mixin class TimestampLogger{
+  DateTime get createdAt => DateTime.now();
+
+  void log(String message) {
+    print('${createdAt.toIso8601String()}: $message');
+  }
+}
+
+class Event with TimestampLogger {
+  String name;
+  Event(this.name);
+}
